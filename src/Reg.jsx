4dc068e90e25a517ddab16e29ece7dc646b23c2e
Vaishnavi <bhandarivaishnavi128@gmail.com>
2024-03-29 23:57:18 +0530

    Updated

--- src/Reg.jsx
+++ src/Reg.jsx
@@ -3,24 +3,79 @@ import ReactDOM from "react-dom/client";
 import './Reg.css'
 function Register()
 {
+    const [formData,setFormData] = React.useState({
+        username:"",
+        email:"",
+        password:""
+    })
+    console.log(formData)
+    function handleChange(event)
+    {
+        const {name,value} = event.target
+        setFormData(prevFormData=>{
+            return{
+                ...prevFormData,
+                [name] : value
+            }
+            
+        })
+    }
+    function handleSubmit(event)
+    {
+        event.preventDefault()
+        fetch('http://127.0.0.1:5000/register',
+        {
+            method:'POST',
+            headers:
+            {
+                'Content-Type' : "application/json", 
+            },
+            body:JSON.stringify(formData),
+        })
+        .then(response=>response.json())
+        .then(data=>{
+            console.log('Success',data)
+        })
+        .catch(error=>{
+            console.error('Error',error)
+        })
+    }
     return(
-        <div class="container">
+        <div className="container">
         <h2>Registration Form</h2>
-        <form>
-            <div class="form-group">
-                <label for="username">Username:</label>
-                <input type="text" id="username" name="username" required/>
+        <form onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label htmlFor="username">Username:</label>
+                <input 
+                type="text" 
+                id="username" 
+                name="username" 
+                onChange={handleChange}
+                value={formData.username}
+                required/>
             </div>
-            <div class="form-group">
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="email" required/>
+            <div className="form-group">
+                <label htmlFor="email">Email:</label>
+                <input 
+                type="email" 
+                id="email" 
+                name="email" 
+                onChange={handleChange}
+                value={formData.email}
+                required/>
             </div>
-            <div class="form-group">
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" required/>
+            <div className="form-group">
+                <label htmlFor="password">Password:</label>
+                <input 
+                type="password" 
+                id="password" 
+                name="password" 
+                onChange={handleChange}
+                value={formData.password}
+                required/>
             </div>
-            <div class="form-group">
-                <button type="submit" onclick="return register()">Register</button>
+            <div className="form-group">
+                <button type="submit">Register</button>
             </div>
         </form>
     </div>
